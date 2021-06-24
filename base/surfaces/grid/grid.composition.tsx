180a20abe0f1934314686fb2a04d2a58c46420c1
Dimbinirina <dimbinirina@icloud.com>
2021-06-24 16:27:29 +0300

    Adding Grid component

---
+++ base/surfaces/grid/grid.composition.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {Grid} from './grid';
+
+export const BasicGrid = () => (
+  <Grid container spacing='lg'>
+      <Grid item>
+          <div>
+              Item 1
+          </div>
+      </Grid>
+      <Grid item>
+          <div>
+              Item 1
+          </div>
+      </Grid>
+      <Grid item>
+          <div>
+              Item 1
+          </div>
+      </Grid>
+  </Grid>
+);
--- base/utils/initials/initials.tsx
+++ base/utils/initials/initials.tsx
@@ -1,12 +1,12 @@
 export function initials(name: string) {
-  if (!name) return undefined;
+    if (!name) return undefined;
 
-  const words = name.split(' ');
-  if (words.length > 1) {
-    return words
-        .slice(0, 2)
-        .map((word) => word[0])
-        .join('');
-  }
-  return name.slice(0, 2);
+    const words = name.split(' ');
+    if (words.length > 1) {
+        return words
+            .slice(0, 2)
+            .map((word) => word[0])
+            .join('');
+    }
+    return name.slice(0, 2);
 }
