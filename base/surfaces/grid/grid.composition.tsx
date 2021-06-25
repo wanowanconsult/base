caa7e7101e4ad23ece19f0856c3b9bc00b5c5b50
Dimbinirina <dimbinirina@icloud.com>
2021-06-25 12:05:32 +0300

    Editing grid

--- base/surfaces/grid/grid.composition.tsx
+++ base/surfaces/grid/grid.composition.tsx
@@ -2,21 +2,9 @@ import * as React from 'react';
 import {Grid} from './grid';
 
 export const BasicGrid = () => (
-  <Grid container spacing='lg'>
-      <Grid item>
-          <div>
-              Item 1
-          </div>
-      </Grid>
-      <Grid item>
-          <div>
-              Item 1
-          </div>
-      </Grid>
-      <Grid item>
-          <div>
-              Item 1
-          </div>
-      </Grid>
+  <Grid>
+      <div>Item 1</div>
+      <div>Item 1</div>
+      <div>Item 1</div>
   </Grid>
 );
--- base/surfaces/grid/grid.tsx
+++ base/surfaces/grid/grid.tsx
@@ -1,69 +1,14 @@
 import * as React from 'react';
-import classnames from 'classnames';
 
 // @ts-ignore
 import styles from "./grid.module.scss";
 
-type Cols = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
-type Spacing = "sm" | "md" | "lg";
+export type GridProps = {} & React.HTMLAttributes<HTMLDivElement>;
 
-type JustifyContent = "flex-start" | "center" | "flex-end" | "space-between";
-type AlignItems = "flex-start" | "center" | "flex-end";
-
-export type GridProps = {
-    /**
-     * Define if the grid is a container.
-     */
-    container?: boolean;
-
-    /**
-     * Define if the grid is an item.
-     */
-    item?: boolean;
-
-    xs?: Cols;
-    sm?: Cols;
-    md?: Cols;
-    lg?: Cols;
-    spacing?: Spacing;
-    justifyContent?: JustifyContent;
-    alignItems?: AlignItems;
-} & React.HTMLAttributes<HTMLDivElement>;
-
-export function Grid({
-                         container,
-                         item,
-                         xs,
-                         sm,
-                         md,
-                         lg,
-                         spacing,
-                         justifyContent,
-                         alignItems,
-                         children,
-                         ...props
-                     }: GridProps) {
-    const classNames = classnames({
-        [styles.Grid_container]: container,
-        [styles.Grid_item]: item,
-        [styles[`Grid_xs_${xs}`]]: xs,
-        [styles[`Grid_sm_${sm}`]]: sm,
-        [styles[`Grid_md_${md}`]]: md,
-        [styles[`Grid_lg_${lg}`]]: lg,
-        [styles[`Grid_spacing_${spacing}`]]: spacing,
-        [styles[`Grid_justifyContent_${justifyContent}`]]: justifyContent,
-        [styles[`Grid_alignItems_${alignItems}`]]: alignItems,
-    });
-
+export function Grid({children, ...props}: GridProps) {
     return (
-      <div className={classNames} {...props}>
+      <div className={styles.Grid} {...props}>
           {children}
       </div>
     );
-}
-
-Grid.defaultProps = {
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    spacing: 'xs',
 }
